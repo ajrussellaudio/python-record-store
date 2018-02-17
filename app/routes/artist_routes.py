4adf20a23b4ef493877bcf803746d207e16838f3
Alan Russell <ajrussellaudio@gmail.com>
2018-02-17 23:08:07 +0000

    add nav link to artist index

--- app/routes/artist_routes.py
+++ app/routes/artist_routes.py
@@ -5,4 +5,4 @@ from app.models.artist import Artist
 @app.route("/artists")
 def artists_index():
     artists = [Artist("Radiohead"), Artist("Photek"), Artist("Lightning Bolt"), Artist("Rolling Stones")]
-    return render_template('artists/index.html', artists=artists)
+    return render_template('artists/index.html', title="All Artists", artists=artists)
