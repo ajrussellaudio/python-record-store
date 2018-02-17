5a9173fd4e2867562aa4278ec5f78f9b054c311f
Alan Russell <ajrussellaudio@gmail.com>
2018-02-17 22:56:17 +0000

    encapsulate routes

---
+++ app/routes/__init__.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+
+@app.route("/")
+def index():
+    return render_template('index.html', title='Home', text='Welcome to Big Al\'s Record Store!')
+
+from app.routes import artist_routes
--- app/routes/artist_routes.py
+++ app/routes/artist_routes.py
@@ -2,11 +2,7 @@ from flask import render_template
 from app import app
 from app.models.artist import Artist
 
-@app.route("/")
-def index():
-    return render_template('index.html', title='Home', text='Welcome to Big Al\'s Record Store!')
-
 @app.route("/artists")
-def artists():
+def artists_index():
     artist = Artist("Radiohead")
     return artist.shout_name()
