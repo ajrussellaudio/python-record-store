e2fd19c6bde10245e333031548754773f3b089f0
Alan Russell <ajrussellaudio@gmail.com>
2018-02-17 22:47:01 +0000

    add artist model

---
+++ app/models/artist.py
@@ -0,0 +1,6 @@
+class Artist():
+    def __init__(self, name):
+        self.name = name
+
+    def shout_name(self):
+        return self.name.upper()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
+from app.models.artist import Artist
 
 @app.route("/")
 def index():
     return render_template('index.html', title='Home', text='Welcome to Big Al\'s Record Store!')
+
+@app.route("/artists")
+def artists():
+    artist = Artist("Radiohead")
+    return artist.shout_name()
