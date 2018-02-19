a5789cdb12dfd65a18d6e346ea0791db8d9a3fe2
Alan Russell <ajrussellaudio@gmail.com>
2018-02-19 10:40:34 +0000

    add new album route

--- app/routes/album_routes.py
+++ app/routes/album_routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
 from app.models.album import Album
+from app.models.artist import Artist
 
 @app.route("/albums")
 def albums_index():
     albums = Album.all()
     return render_template('albums/index.html', albums=albums)
+
+@app.route("/albums/new")
+def albums_new():
+    artists = Artist.all()
+    return render_template('albums/new.html', artists=artists)
