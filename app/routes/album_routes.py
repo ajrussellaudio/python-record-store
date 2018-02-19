f2a45ae9d2409442bed7eca5651dbc10b35d83e7
Alan Russell <ajrussellaudio@gmail.com>
2018-02-19 10:50:02 +0000

    add create album route

--- app/routes/album_routes.py
+++ app/routes/album_routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
 from app.models.album import Album
 from app.models.artist import Artist
@@ -12,3 +12,11 @@ def albums_index():
 def albums_new():
     artists = Artist.all()
     return render_template('albums/new.html', artists=artists)
+
+@app.route("/albums", methods=["POST"])
+def albums_create():
+    title = request.form['title']
+    artist_id = int(request.form['artist_id'])
+    album = Album(title=title, artist_id=artist_id)
+    album.save()
+    return redirect("/albums")
